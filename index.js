89ca535c391546b470dfe7b71d1d23ac26498f49
reisvictor1 <reisvictor448@gmail.com>
2021-01-16 18:42:40 -0300

    refactor the base model

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = 5000
 const app = express()
 
 
+mongoose.connect('mongodb+srv://adminccr:teamccr33@team33.rtgyi.mongodb.net/dbccr33?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true})
+
 app.listen(PORT, () => {
     console.log(`Escutando na porta ${PORT}`)
 })
