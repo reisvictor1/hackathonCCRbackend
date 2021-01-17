0a4c84da71589db7218d919dd6a41a84495ffbc2
reisvictor1 <reisvictor448@gmail.com>
2021-01-17 15:09:26 -0300

    add feedback and addVideo functions into course controller

--- controllers/course.js
+++ controllers/course.js
@@ -1,5 +1,5 @@
 const courseModel = require('../models/course')
-
+const studentModel = require('../models/student')
 
 module.exports.getOneCourse = async (req, res) => {
 
@@ -33,7 +33,9 @@ module.exports.createCourse = async (req, res) => {
         description: description,
         ementa: ementa,
         number_videos: 0,
-        student_pre_requisites: student_pre_requisites
+        student_pre_requisites: student_pre_requisites,
+        feedbacks: [],
+        videos: []
     })
 
     const courseCreated = await newCourse.save()
@@ -54,4 +56,53 @@ module.exports.deleteCourse = async (req, res) => {
     if(!deletedCourse) return res.status(400).send('Não foi possível deletar o curso.')
 
     return res.status(200).json(deletedCourse)
+}
+
+module.exports.addVideo = async (req, res) => {
+    const {name, title, description, link} = req.body
+
+    const course = await courseModel.find({name: name})
+
+    if(!course) return res.status(400).send('Não foi possível encontrar este curso.')
+
+    let { number_videos } = course
+
+    console.log(number_videos)
+
+    await course.videos.push({
+        title: title,
+        description: description,
+        link: link
+    })
+
+    const videoAdded = await course.save()
+
+    if(!videoAdded) return res.status(400).send('Não foi possível adicionar o vídeo ao curso') 
+
+    const courseUpdated = await course.update({number_videos: number_videos+1})
+    
+    if(!courseUpdated) return res.status(400).send('Não foi possível atualizar o curso.')
+
+    return res.status(200).json(courseUpdated)
+}
+
+
+module.exports.addFeedback = async (req,res) => {
+
+    const { email, name } = req.query
+    const { feedback , rank } = req.body
+
+    const course = await courseModel.findOne({ name: name })
+    const student = await studentModel.findOne({ email: email })
+
+   await course.feedbacks.push({
+       student: student,
+       feedback: feedback,
+       rank: rank
+   })
+
+   await course.save()
+
+   return res.status(200).json(course)
+
 }
