1d108b54ad8ec0c344d659951106f659d7d1d835
reisvictor1 <reisvictor448@gmail.com>
2021-01-17 21:04:05 -0300

    refactor(master): change how the port is acessed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-const PORT = 5000
+const PORT =process.env.PORT  || 5000
 
 const app = express()
 
